0c9c581867cd58e0711082c5860e83c976c35860
reign <RY8GN@proton.me>
2026-02-21 19:30:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function toggleTheme() {
     btn.textContent = '☾';
     localStorage.setItem('theme', 'light');
     // Light theme colors
-    iframe.src = 'https://commit-palette.vercel.app?username=unitreign&colors=d4cfc6,d4c9e8,b5a0d4,9478bc,7c4dba,1a1a1a&months=6';
+    iframe.src = 'https://commit-palette.vercel.app?username=unitreign&colors=d4cfc6,d4c9e8,b5a0d4,9478bc,7c4dba,1a1a1a&months=4';
   } else {
     html.dataset.theme = 'dark';
     btn.textContent = '☀';
     localStorage.setItem('theme', 'dark');
     // Dark theme colors
-    iframe.src = 'https://commit-palette.vercel.app?username=unitreign&colors=33312c,2d2848,4a3d72,6b56a0,9b6fd4,e8e4dc&months=6';
+    iframe.src = 'https://commit-palette.vercel.app?username=unitreign&colors=33312c,2d2848,4a3d72,6b56a0,9b6fd4,e8e4dc&months=4';
   }
 }
 
